023926826e8110d4824d0ed7948bd44f69ab96cf
= <obsidize@gmail.com>
2025-06-16 10:24:56 -0500

    fix typedoc warning

--- src/index.ts
+++ src/index.ts
@@ -7,17 +7,20 @@ export {
 	LogEventSerializerDelegate,
 	LogEventSerializerDelegateConfig,
 	LogEventSerializerLike,
-	LogEventSerializerPropertyFormatter
+	LogEventSerializerPropertyFormatter,
 } from './core/log-event-serializer';
-export {
-	LogEventTransport,
-	LogEventTransportConfig
-} from './core/log-event-transport';
+export { LogEventTransport, LogEventTransportConfig } from './core/log-event-transport';
 export { LogLevel } from './core/log-level';
 export { Logger } from './core/logger';
-export type { ConsoleLike, LogEventConsumer, LogEventInterceptor, LogEventLike, LogEventProducer } from './core/types';
+export type {
+	ConsoleLike,
+	LogEventConsumer,
+	LogEventInterceptor,
+	LogEventLike,
+	LogEventProducer,
+	Maybe,
+} from './core/types';
 export { getDefaultTransport, log } from './defaults';
 export { consoleOutput, ConsoleOutputConfig, ConsoleOutputInvoker } from './outputs/console-output';
 export { interceptorOutput } from './outputs/interceptor-output';
 export { serializerOutput, SerializerOutputConfig } from './outputs/serializer-output';
-
